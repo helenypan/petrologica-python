2ba652aff2e998487b7ae60a9ce4016959579fa7
helenypan <yue.pan@me.com>
2017-05-18 22:28:38 +0100

    finish the function of downloading well index data

--- wells/get_well_index.py
+++ wells/get_well_index.py
@@ -1,97 +1,63 @@
 import sys
 sys.path.append('../includes')
 from db import DB
+from io import BytesIO
+from io import TextIOWrapper
+import requests
+from zipfile import ZipFile
+import csv
 from datetime import datetime
-from bs4 import BeautifulSoup
-import urllib.request
-import ssl
-import re
-context = ssl._create_unverified_context()
-url_to_complete = "https://uk.finance.yahoo.com/quote/{}/history?interval=1wk&filter=history&frequency=1wk"
-url_ftse = "https://uk.investing.com/indices/uk-100-historical-data"
+import url_info
 
 db = DB()
 conn = db.conn
 cur = db.cur
 
-def extract_ftse_data(url, dates):
-	print(dates)
-	req = urllib.request.Request(url, headers={'User-Agent': 'Mozilla/5.0'})
-	# html = urlopen(req,, context = context).read()
-	with urllib.request.urlopen(req, context = context) as url_file:
-		html = str(url_file.read())
-		html_list = re.split("<html |</html>",html)
-		html_to_parse = "<html " + html_list[1] + "</html>"
-		# print(html_to_parse)
-		soup = BeautifulSoup(html_to_parse,'html.parser')
-		tbl_list = soup.find_all("table")
-	for tbl in tbl_list:
-		tbl_id = tbl.get("id")
-		if tbl_id == "curr_table":
-			rows = tbl.find_all("tr")
-			counter = 0
-			for row in rows:
-				if counter >0 and counter <15:
-					cells = row.find_all("td")
-					dt_text = cells[0].getText()
-					index_data = cells[1].getText()
-					index_val = float(re.sub(',', '', index_data))
-					dt = str(datetime.strptime(dt_text,"%b %d, %Y")).split()[0]
-					if dt in dates:
-						save_to_db("FTSE100",dt, index_val)
-				counter = counter + 1
-	conn.commit()
 
+def update_well_index_in_db(row):
+	well_number = row['FileNo']
+	cur.execute('''delete from 
+		tomorrow_wells_new.well_index where FileNo = %s''',(well_number,))
+	sql = "insert into tomorrow_wells_new.well_index set"
+	for idx, val in row.items():
+		if idx == "SpudDate" or idx == "WellStatusDate":
+			# need to convert the format of the date
+			if "/" in val:
+				dt_obj = datetime.strptime(val, '%m/%d/%Y')
+				val = dt_obj.strftime("%Y-%m-%d")
+			else:
+				val = None
+		else:
+			val = val.replace('"','')
+		sql += ' `{}` = "{}",'.format(idx, val)
+	sql = sql.strip(",")
+	cur.execute(sql)
+	# if well_number == "15":
+	# 	print(sql)
+	print("Well saved:",well_number)
 
-def save_to_db(company_code, dt, val):
-	cur.execute('''insert ignore into 
-		tomorrow_external_data.index_data(company_code, date, value) 
-		VALUES(%s,%s,%s)''',(company_code, dt, val))
-	print("New record saved:",(company_code, dt, val))
+url_file = requests.get(url_info.well_index_url, auth=(url_info.username, url_info.password)).content
+zip_file = ZipFile(BytesIO(url_file))
+file_name = zip_file.namelist()[0]
+item_file = TextIOWrapper(zip_file.open(file_name,"r"))
 
-def extract_company_prices(url):
-	dt_list = list()
-	with urllib.request.urlopen(url, context=context) as url_file:
-		html = url_file.read()
-		soup = BeautifulSoup(html, "html.parser")
-		tbl_list = soup.find_all('table')
-	for tbl in tbl_list:
-		data_test_lbl = tbl.get("data-test")
-		if data_test_lbl == "historical-prices":
-			#found the data
-			rows = tbl.find_all("tr")
-			counter = 0
-			for row in rows:
-				if counter >0 and counter < 3:
-					cells = row.find_all("td")
-					dt_text = cells[0].getText()
-					close_val = float(cells[4].getText())
-					dt = str(datetime.strptime(dt_text,"%d %b %Y")).split()[0]
-					if dt not in dt_list:
-						dt_list.append(dt)
-					save_to_db(company_code,dt, close_val)
-				counter = counter + 1
-	conn.commit()
-	return dt_list
-
-
-company_codes = list()
-dates_to_extract = None
-cur.execute('''select company_code from 
-	tomorrow_external_data.index_company order by company_code''')
-for row in cur.fetchall():
-	company_codes.append(row[0])
-
-for company_code in company_codes:
-	url = url_to_complete.format(company_code)
-	if dates_to_extract is None:
-		dates_to_extract = extract_company_prices(url)
-	else:
-		extract_company_prices(url)
-# now need to extract ftse100 data of the dates in dates_to_extract
-extract_ftse_data(url_ftse,dates_to_extract)
+reader = csv.DictReader(item_file)
+counter = 0 
+for row in reader:
+	update_well_index_in_db(row)
+	counter += 1
+	if counter % 50 == 0:
+		conn.commit()
+conn.commit()
 cur.close()
+		
+  	
+
+# for idx, row in enumerate(csv.DictReader(item_file)):
+#     print('Processing row {0} -- row = {1}'.format(idx, row))
 
 
-
-
+# with zip_file.open(file_name,"r") as f:
+# 	reader = csv.reader(f)
+# 	your_list = list(reader)
+# print(your_list[:5])
