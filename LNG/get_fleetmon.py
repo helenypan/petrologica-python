bc157da109f7d7563e237ab824b527a6c4ff40f5
helenypan <yue.pan@me.com>
2017-05-25 22:59:19 +0100

    correct get fleet mon script, to be continued

--- LNG/get_fleetmon.py
+++ LNG/get_fleetmon.py
@@ -38,15 +38,33 @@ def update_well_index_in_db(row):
 
 
 cur_time  = datetime.now().strftime("%Y%m%dH%H")
-file_name = "./archive/" + cur_time + ".html"
+file_name = "./archive/" + cur_time + ".csv"
 dir_name =  os.path.dirname(file_name)
 if not os.path.exists(dir_name):
     os.makedirs(dir_name)
 
-s = requests.Session() 
-data={"username":url_info.username, "password":url_info.password}
-r1 = s.post(url_info.login_url,data=data)
-response = s.get(url_info.tracker_url,data=data, cookies=r1.cookies)
+headers ={
+	"Host": "www.fleetmon.com",
+	"Connection": "keep-alive",
+	"Upgrade-Insecure-Requests": "1",
+	"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
+	"Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+	"Referer": "https://www.fleetmon.com/my/vessels/",
+	"Accept-Encoding": "gzip, deflate, sdch, br",
+	"Accept-Language": "en-GB,en-US;q=0.8,en;q=0.6,zh-CN;q=0.4,zh-TW;q=0.2",
+	"Cookie": "intercom-id-kwshk9to=f237a359-ceff-48fa-8ecd-511b1b78f407; _hjIncludedInSample=1;\
+csrftoken=Q8L2H0vKLlI5dcLeiaiXqVdH63RKGMSb; ajs_anonymous_id=null; \
+_ga=GA1.2.373668620.1495546644; _gid=GA1.2.846791572.1495747134; _gat=1;\
+intercom-session-kwshk9to=cWlTVmVkbTAyMkxCRDhuSU1mVlFsOE94Q3duMmRjcTM3NXFtR2lJd0ErU1hJMGVqc0dsbUtMZytkQnlqWGE3bC0tV2d1eEFqSTZ4QWhtWHRPa1dncHBBUT09--ea2808ba991daa5ba1d8f8e82bcd9b744a3dc762;\
+fmc_session=6aqgobkyg8ehxm9skcy4skl8ct1itzt0; _gali=download-export-module"
+}
+
+
+
+# s = requests.Session() 
+# data={"username":url_info.username, "password":url_info.password}
+# r1 = s.post(url_info.login_url,data=data)
+response = requests.get(url_info.tracker_url,headers=headers)
 with open(file_name, 'wb') as f:
 	f.write(response.content)
 
