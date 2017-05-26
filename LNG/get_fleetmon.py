2acb2d614778f349d756ede1ff8b208ba4f36ba3
helenypan <yue.pan@me.com>
2017-05-27 00:53:05 +0100

    update get fleetmon function to mock login

--- LNG/get_fleetmon.py
+++ LNG/get_fleetmon.py
@@ -1,47 +1,12 @@
 import sys
 sys.path.append('../includes')
-from db import DB
-from io import BytesIO
-from io import TextIOWrapper
 import requests
-import csv
+import urllib
+import ssl
 from datetime import datetime
 import url_info
 import os
 
-# db = DB()
-# conn = db.conn
-# cur = db.cur
-
-
-def update_well_index_in_db(row):
-	well_number = row['FileNo']
-	cur.execute('''delete from 
-		tomorrow_wells_new.well_index where FileNo = %s''',(well_number,))
-	sql = "insert into tomorrow_wells_new.well_index set"
-	for idx, val in row.items():
-		if idx == "SpudDate" or idx == "WellStatusDate":
-			# need to convert the format of the date
-			if "/" in val:
-				dt_obj = datetime.strptime(val, '%m/%d/%Y')
-				val = dt_obj.strftime("%Y-%m-%d")
-			else:
-				val = None
-		else:
-			val = val.replace('"','')
-		sql += ' `{}` = "{}",'.format(idx, val)
-	sql = sql.strip(",")
-	cur.execute(sql)
-	# if well_number == "15":
-	# 	print(sql)
-	print("Well saved:",well_number)
-
-
-cur_time  = datetime.now().strftime("%Y%m%dH%H")
-file_name = "./archive/" + cur_time + ".csv"
-dir_name =  os.path.dirname(file_name)
-if not os.path.exists(dir_name):
-    os.makedirs(dir_name)
 
 headers ={
 	"Host": "www.fleetmon.com",
@@ -59,27 +24,49 @@ intercom-session-kwshk9to=cWlTVmVkbTAyMkxCRDhuSU1mVlFsOE94Q3duMmRjcTM3NXFtR2lJd0
 fmc_session=6aqgobkyg8ehxm9skcy4skl8ct1itzt0; _gali=download-export-module"
 }
 
+cur_time  = datetime.now().strftime("%Y%m%dH%H")
+file_name = "./archive/" + cur_time + ".csv"
+dir_name =  os.path.dirname(file_name)
+if not os.path.exists(dir_name):
+    os.makedirs(dir_name)
 
+s = requests.Session() 
+login_data={"username":url_info.username, "password":url_info.password}
 
-# s = requests.Session() 
-# data={"username":url_info.username, "password":url_info.password}
-# r1 = s.post(url_info.login_url,data=data)
-response = requests.get(url_info.tracker_url,headers=headers)
-with open(file_name, 'wb') as f:
-	f.write(response.content)
+login_header ={
+	"Host": "www.fleetmon.com",
+	"Connection": "keep-alive",
+	"Content-Length": "97",
+	"Cache-Control": "max-age=0",
+	"Origin": "https://www.fleetmon.com",
+	"Upgrade-Insecure-Requests": "1",
+	"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
+	"Content-Type": "application/x-www-form-urlencoded",
+	"Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+	"Referer": "https://www.fleetmon.com/users/login/",
+	"Accept-Encoding": "gzip, deflate, br",
+	"Accept-Language": "en-GB,en-US;q=0.8,en;q=0.6,zh-CN;q=0.4,zh-TW;q=0.2",
+	"Cookie": "intercom-id-kwshk9to=f237a359-ceff-48fa-8ecd-511b1b78f407; _hjIncludedInSample=1; fmc_session=4zwr88j3a1bb0ygcff042721f8ciebl3; csrftoken=ew9eCGfd9wS97JA2IM0LR1CE0GOl9W84; ajs_user_id=null; ajs_group_id=null; ajs_anonymous_id=%2296fe625f-25e3-42aa-a545-78b4d38f6435%22; _ga=GA1.2.373668620.1495546644; _gid=GA1.2.1465568261.1495837822; _gat=1; intercom-session-kwshk9to=NVZvWlFOUTE2RVRpc3BxS2VTS0pZa25ZNzlZbHViK2FTb2tUM3BXRkRRUnZhaHpYdndLMGYyUUhSVml3NGZRby0tRUNDZHZMOFFqOGFZYnRjNlJHVUdCQT09--9c50b292baca9a7e9907347812cc60bf45177cde; _gali=login_form"
+}
 
-# zip_file = ZipFile(BytesIO(url_file))
-# file_name = zip_file.namelist()[0]
-# item_file = TextIOWrapper(zip_file.open(file_name,"r"))
+context = ssl._create_unverified_context()
+data = urllib.parse.urlencode(login_data).encode()
+hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+       'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+       'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+       'Accept-Encoding': 'none',
+       'Accept-Language': 'en-US,en;q=0.8',
+       'Connection': 'keep-alive'}
+req = urllib.request.Request(url_info.login_url,data=data, headers=hdr)
+try:
+	f = request.urlopen(req, context = context)
+except e:
+	print(e.fp.read())
 
-# reader = csv.DictReader(item_file)
-# counter = 0 
-# for row in reader:
-# 	update_well_index_in_db(row)
-# 	counter += 1
-# 	if counter % 50 == 0:
-# 		conn.commit()
-# conn.commit()
-# cur.close()
+# r1 = requests.post(url_info.login_url,data=data, headers = login_header)
+# print(r1.content)
+# response = requests.get(url_info.tracker_url,headers=headers)
+# with open(file_name, 'wb') as f:
+# 	f.write(response.content)
 		
   	
